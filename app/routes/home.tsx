24c1ef348bf1271d8fffb4006eb92eab51abb83b
OneLessLife <jason132125@gmail.com>
2026-01-06 21:53:36 +0200

    Authentication

---
+++ app/routes/home.tsx
@@ -0,0 +1,44 @@
+import Navbar from "~/components/Navbar";
+import type { Route } from "./+types/home";
+import { resumes } from "/.react-router/types/constants";
+import ResumeCard from "~/components/ResumeCard";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "OneLessResume" },
+    { name: "description", content: "Smart feedback for your dream job!" },
+  ];
+}
+
+export default function Home() {
+  const { isLoading, auth } = usePuterStore();
+  const location = useLocation();
+  const next = location.search.split('next=')[1];
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!auth.isAuthenticated) navigate('/auth?next=/') ;
+  },[auth.isAuthenticated]);
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        <h1>Review Your Submissions and check AI-powered feedback</h1>
+        </div>
+    {resumes.length > 0 && (
+      <div className="resumes-section">
+      {resumes.map((resume: typeof resumes[number]) => (
+            <ResumeCard key={resume.id} resume={resume} />
+          ))}
+      </div>
+    )}
+
+      </section>
+    </main> 
+  ;
+}
